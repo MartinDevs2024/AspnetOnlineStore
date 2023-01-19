9e7ae5deb79f165cd73e49d2c25bc9cf8307e3e0
Martin Fernandez <Electronicsmf@aol.com>
2023-01-19 07:07:53 -0500

    Adding Slide Images

--- OrOnlineStore/Areas/UI/Controllers/HomeController.cs
+++ OrOnlineStore/Areas/UI/Controllers/HomeController.cs
@@ -1,10 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using OrOnlineStore.DataAccess.Repository.IRepository;
+using OrOnlineStore.Models;
 using OrOnlineStore.Models.ViewModels;
+using OrOnlineStore.Utility;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace OrOnlineStore.Areas.UI.Controllers
@@ -13,16 +20,23 @@ namespace OrOnlineStore.Areas.UI.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _hostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, 
+            IUnitOfWork unitOfWork, 
+            IWebHostEnvironment hostEnvironment)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
+            _hostEnvironment = hostEnvironment;
         }
 
         public IActionResult Index()
         {
-            return View();
+            return View(); 
         }
+
 
         public IActionResult About()
         {
