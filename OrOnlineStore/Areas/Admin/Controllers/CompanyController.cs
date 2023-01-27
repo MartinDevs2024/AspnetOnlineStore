4e4a99611cabb18ad09489f3a3ecaa9a0b0d1f0a
Martin Fernandez <Electronicsmf@aol.com>
2023-01-27 06:47:54 -0500

    Creating the company Controller and the view

--- OrOnlineStore/Areas/Admin/Controllers/CompanyController.cs
+++ OrOnlineStore/Areas/Admin/Controllers/CompanyController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using OrOnlineStore.DataAccess.Repository.IRepository;
 using OrOnlineStore.Models;
 
@@ -19,36 +20,62 @@ namespace OrOnlineStore.Areas.Admin.Controllers
             return View();
         }
 
-       
+        //GET
+        public IActionResult Upsert(int? id)
+        {
+            Company company = new();
+            if (id == null || id == 0)
+            {
+                return View(company);
+            }
+            else
+            {
+                company = _unitOfWork.Company.GetFirstOrDefault(u => u.Id ==id);
+                return View(company);
+            }
+        }
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Company company)
+        public IActionResult Upsert(Company obj, IFormFile file)
         {
             if (ModelState.IsValid)
             {
-                if (company.Id == 0)
+                if (obj.Id == 0)
                 {
-                    _unitOfWork.Company.Add(company);
+                    _unitOfWork.Company.Add(obj);
+                    TempData["success"] = "Company created successfully";
                 }
                 else
                 {
-                    _unitOfWork.Company.Update(company);
+                    _unitOfWork.Company.Update(obj);
                 }
                 _unitOfWork.Save();
                 return RedirectToAction(nameof(Index));
             }
-            return View(company);
+            return View(obj);
         }
 
-        #region
+        #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
-            var allObj = _unitOfWork.Company.GetAll();
-            return Json(new { data = allObj });
+            var companyList = _unitOfWork.Company.GetAll();
+            return Json(new { data = companyList });
         }
 
-      
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var obj = _unitOfWork.Company.GetFirstOrDefault(u => u.Id == id);
+            if (obj == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+            _unitOfWork.Company.Remove(obj);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Delete Successful" });
+        }
         #endregion
     }
 }
